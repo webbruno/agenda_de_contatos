8072eb7f83b2704acc760888101ca8990f5ee866
Bruno Costa <bdasilvacosta@gmail.com>
2023-08-21 23:18:33 -0300

    Update agenda.py
    Criado menu da agenda com if e while

--- agenda.py
+++ agenda.py
@@ -24,6 +24,7 @@ def buscar_contato(contato):
     print(f'Telefone: {AGENDA[contato]["telefone"]}')
     print(f'email: {AGENDA[contato]["email"]}')
     print(f'Endereço: {AGENDA[contato]["endereco"]}')
+    print('--------------------')
 
 
 def incluir_editar_contato(nome, telefone, email, endereco):
@@ -40,8 +41,39 @@ def excluir_contato(contato):
     print(f'>>>>Contato {contato} excluido com sucesso')
 
 
-mostrar_contatos()
-incluir_editar_contato('Sandra','11965102064','sandra.pcosta@gmail.com','Teodoro Bernardo do Nascimento')
-mostrar_contatos()
-excluir_contato('Bruno')
-mostrar_contatos()
+def imprimir_menu():
+    print('1 - Mostrar todos os contatos da agenda')
+    print('2 - Buscar contato')
+    print('3 - Incluir contato')
+    print('4 - Editar contato')
+    print('5 - Excluir contato')
+    print('0 - Fechar o programa')
+
+
+if __name__ == '__main__':
+    while True:
+        imprimir_menu()
+        print('--------------------')
+        opcao = input('Escolha uma opção: ')
+        print('--------------------')
+        if opcao == '1':
+            mostrar_contatos()
+        elif opcao == '2':
+            contato = input('Digite o nome do contato: ')
+            print('--------------------')
+            buscar_contato(contato)
+        elif opcao == '3' or opcao == '4':
+            contato = input('Digite o nome do contato: ')
+            telefone = input('Digite o telefone: ')
+            email = input('Digite o email: ')
+            endereco = input('Digite o endereço: ')
+            incluir_editar_contato(contato, telefone, email, endereco)
+        elif opcao == '5':
+            contato = input('Digite o nome do contato: ')
+            excluir_contato(contato)
+        elif opcao == '0':
+            print('Fechando o programa')
+            break
+        else:
+            print('Opção inválida')
+
