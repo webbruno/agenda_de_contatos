84901dc8c6d96f5246e10c01febef1516568b267
Bruno Costa <bdasilvacosta@gmail.com>
2023-08-23 21:32:12 -0300

    Tratamento de erros
    Adicionado tratamento de erros no código e refatorada a função incluir_editar_contatos

--- agenda.py
+++ agenda.py
@@ -14,31 +14,63 @@ AGENDA['Andre'] = {
 
 
 def mostrar_contatos():
-    for contato in AGENDA:
-        buscar_contato(contato)
-        print('--------------------')
+    if AGENDA:
+        for contato in AGENDA:
+            buscar_contato(contato)
+    else:
+        print()
+        print('>>>> Agenda vazia')
+        print()
 
 
 def buscar_contato(contato):
-    print(f'Nome: {contato}')
-    print(f'Telefone: {AGENDA[contato]["telefone"]}')
-    print(f'email: {AGENDA[contato]["email"]}')
-    print(f'Endereço: {AGENDA[contato]["endereco"]}')
-    print('--------------------')
+    try:
+        print(f'Nome: {contato}')
+        print(f'Telefone: {AGENDA[contato]["telefone"]}')
+        print(f'email: {AGENDA[contato]["email"]}')
+        print(f'Endereço: {AGENDA[contato]["endereco"]}')
+        print('----------------------------------------')
+    except KeyError:
+        print()
+        print(f'>>>> Contato {contato} inexistente')
+        print()
+    except Exception as error:
+        print()
+        print('>>>> Um erro inesperado ocorreu')
+        print(error)
+        print()
 
 
-def incluir_editar_contato(nome, telefone, email, endereco):
-    AGENDA[nome] = {
+def incluir_editar_contato(contato):
+    telefone = input('Digite o telefone: ')
+    email = input('Digite o email: ')
+    endereco = input('Digite o endereço: ')
+    
+    AGENDA[contato] = {
         'telefone': telefone,
         'email': email,
         'endereco': endereco,
     }
-    print(f'>>>>Contato {nome} adicionado/editado com sucesso\n')
+    print()
+    print(f'>>>> Contato {contato} adicionado/editado com sucesso\n')
+    print()
 
 
 def excluir_contato(contato):
-    AGENDA.pop(contato)
-    print(f'>>>>Contato {contato} excluido com sucesso')
+    try:
+        AGENDA.pop(contato)
+        print()
+        print(f'>>>> Contato {contato} excluido com sucesso')
+        print()
+    except KeyError:
+        print()
+        print(f'>>>> Contato {contato} inexistente')
+        print()
+    except Exception as error:
+        print()
+        print('>>>> Um erro inesperado ocorreu')
+        print(error)
+        print()
 
 
 def imprimir_menu():
@@ -53,21 +85,32 @@ def imprimir_menu():
 if __name__ == '__main__':
     while True:
         imprimir_menu()
-        print('--------------------')
+        print('----------------------------------------')
         opcao = input('Escolha uma opção: ')
-        print('--------------------')
+        print('----------------------------------------')
         if opcao == '1':
             mostrar_contatos()
         elif opcao == '2':
             contato = input('Digite o nome do contato: ')
-            print('--------------------')
+            print('----------------------------------------')
             buscar_contato(contato)
-        elif opcao == '3' or opcao == '4':
+        elif opcao == '3':
             contato = input('Digite o nome do contato: ')
-            telefone = input('Digite o telefone: ')
-            email = input('Digite o email: ')
-            endereco = input('Digite o endereço: ')
-            incluir_editar_contato(contato, telefone, email, endereco)
+            
+            try:
+                AGENDA[contato]
+                print(f'>>>> Ccontato {contato} já existente')
+            except KeyError:
+                incluir_editar_contato(contato)
+        elif opcao == '4':
+            contato = input('Digite o nome do contato: ')
+
+            try:
+                AGENDA[contato]
+                print(f'>>>> Editando contato: {contato}')
+                incluir_editar_contato(contato)
+            except KeyError:
+                print('>>>> Contato inexistente')
         elif opcao == '5':
             contato = input('Digite o nome do contato: ')
             excluir_contato(contato)
